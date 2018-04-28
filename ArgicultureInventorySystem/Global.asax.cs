5bb84139cb01a33bdb91c431b129536e6812d302
RAY ADDERLEY JM. GINING <ray_adderley@staf.uitm.edu.my>
2018-04-28 16:27:57 +0800

    Numbering problem solved for datatable view

--- ArgicultureInventorySystem/Global.asax.cs
+++ ArgicultureInventorySystem/Global.asax.cs
@@ -22,6 +22,7 @@ namespace ArgicultureInventorySystem
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Session.Timeout = 60;
         }
     }
 }
